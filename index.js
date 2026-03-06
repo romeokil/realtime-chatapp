d00e25d058e4776bddf80e1714049a39bc274e06
romeokil <rahulkumarjha58978@gmail.com>
2026-03-07 01:04:21 +0530

    changed the index.js file such that 2 user can connect to each other using websockets and can talk to each other and it basically broadcast this message to all users , which followed for group not for individual chat, for this we will use rooms.

--- index.js
+++ index.js
@@ -1,70 +1,40 @@
 import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
 import http from "http";
+import dotenv from 'dotenv'
 import { Server } from "socket.io";
-
+import cors from "cors";
 dotenv.config();
-
 const app = express();
+const PORT =5000;
 app.use(cors());
-app.use(express.json());
-
-const PORT = process.env.PORT || 5000;
 
 const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "*",
-  },
+    origin: "*"
+  }
 });
 
-
-// ye hmara socket communication hai.
 io.on("connection", (socket) => {
-  console.log("Socket connected:", socket.id);
 
-  socket.on("chat_message", (data, callback) => {
-    const { message } = data;
+  console.log("User connected:", socket.id);
 
-    console.log("Message received:", message);
+  // message recieve kr lete hai.
+  socket.on("send_message", (data) => {
 
-    let response;
+    console.log("Message received from", data.user);
+    console.log("Message:", data.message);
 
-    if (message.toLowerCase() === "hello") {
-      response = "Hi! How can I help you?";
-    } else if (message.toLowerCase() === "how are you") {
-      response = "I am running perfectly!";
-    } else {
-      response = "Sorry, I didn't understand.";
-    }
-    callback({
-      reply: response,
-    });
+    // ab message emit kr dete hai toh jitna bhi user hai sbtk pahuch jaata hai.
+    io.emit("receive_message", data);
   });
 
   socket.on("disconnect", () => {
-    console.log("Socket disconnected:", socket.id);
+    console.log("User disconnected:", socket.id);
   });
+
 });
-
-
-// internal socket client baana rhe hai hmlog.
-import { io as Client } from "socket.io-client";
-
-const internalSocket = Client(`http://localhost:${PORT}`);
-
-
-// API THAT USES SOCKET
-app.post("/api/chat", (req, res) => {
-  const { message } = req.body;
-
-  internalSocket.emit("chat_message", { message }, (response) => {
-    res.json(response);
-  });
-});
-
 
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
